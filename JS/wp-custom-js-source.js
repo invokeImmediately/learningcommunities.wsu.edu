81f073e7a374fa20ae32d0d754804a02e53c310a
Daniel Rieck <danielcrieck@gmail.com>
2016-06-15 22:10:33 -0700

    Updating build
    Rebuilt production files to incorporate recent changes to source code

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -3,56 +3,60 @@
  CUSTOM JQUERY-BASED DYNAMIC CONTENT
  *********************************************************************************************************************/
 (function ($) {
-    "use strict";
-    
-    var $oueActiveInfoPanel;
-    
+	"use strict";
+	
+	var $oueActiveInfoPanel;
+	
 	$(document).ready(function () {
-        /**********************************************************************************************
-         * Tweak HTML source to work around some quirks of WordPress setup                            *
-         **********************************************************************************************/
-        var siteURL = window.location.pathname;
-        switch(siteURL) {
-                // REMOVED FOR NOW: Removal of "home" link when on home page.
+		/**********************************************************************************************
+		 * Tweak HTML source to work around some quirks of WordPress setup                            *
+		 **********************************************************************************************/
+		var siteURL = window.location.pathname;
+		switch(siteURL) {
+				// REMOVED FOR NOW: Removal of "home" link when on home page.
 				/* case '/':
-                $('#menu-item-35').remove();
-                $('#spine-sitenav ul li').first().css('border-top', 'none');
-                $('#spine-sitenav').addClass('homeless');
-                break; */
-            case '/news/':
-                $('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
-                break;
-        }
-        
-        InitResHallCourseInfoPanels("ul.residence-hall-courses", "li.course-info-panel", ".hidden-info");
+				$('#menu-item-35').remove();
+				$('#spine-sitenav ul li').first().css('border-top', 'none');
+				$('#spine-sitenav').addClass('homeless');
+				break; */
+			case '/news/':
+				$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
+				break;
+		}
+		
+		InitResHallCourseInfoPanels("ul.residence-hall-courses", "li.course-info-panel", ".hidden-info");
 	});
-    
-    function InitResHallCourseInfoPanels(slctrPrntList, slctrChldPanels, slctrHiddenInfo) {
-        var $courseLists = $(slctrPrntList);
+	
+	function InitResHallCourseInfoPanels(slctrPrntList, slctrChldPanels, slctrHiddenInfo) {
+		var $courseLists = $(slctrPrntList);
 		$courseLists.each(function () {
 			var $infoPanels = $(this).find(slctrChldPanels);
 			$infoPanels.click(function () {
 				var $this = $(this);
+				var isActive = $this.data("is-active");
 				
-				// Start by noting this as the active panel
-				$this.data("is-active", "1");
-				
-				// Before changing anything, compute and store the current CSS style rules we will change
-				var leftPosCmptd = $this.css("left");
-				var widthCmptd = $this.css("width");
-				var prntWidthCmptd = $this.parent().css("width");
-				var leftStyleSttng = (100 * parseFloat(leftPosCmptd) / parseFloat(prntWidthCmptd));
-				$this.data("prev-left-setting", leftStyleSttng + "%");
-				var widthStyleSttng = (100 * parseFloat(widthCmptd) / parseFloat(prntWidthCmptd));
-				$this.data("prev-width-setting", widthStyleSttng + "%");
-				
-				// Animate the widening of the box and revelation of previously hidden course info
-				var $hiddenInfo = $this.find(slctrHiddenInfo);
-				$this.animate({
-					"width" : "100%",
-					"left" : "0%"
-				}, 333);
-				$hiddenInfo.stop().show(200);
+				if(isActive == undefined || isActive == 0) {
+					// Start by noting this as the active panel
+					$this.data("is-active", "1");
+					
+					// Before changing anything, compute and store the current CSS style rules we will change
+					var leftPosCmptd = $this.css("left");
+					var widthCmptd = $this.css("width");
+					var prntWidthCmptd = $this.parent().css("width");
+					var leftStyleSttng = (100 * parseFloat(leftPosCmptd) / parseFloat(prntWidthCmptd));
+					$this.data("prev-left-setting", leftStyleSttng + "%");
+					var widthStyleSttng = (100 * parseFloat(widthCmptd) / parseFloat(prntWidthCmptd));
+					$this.data("prev-width-setting", widthStyleSttng + "%");
+					
+					// Animate the widening of the box and revelation of previously hidden course info
+					var $hiddenInfo = $this.find(slctrHiddenInfo);
+					$this.animate({
+						"width" : "100%",
+						"left" : "0%"
+					}, 333, (function($objsToShow) {
+						$objsToShow.stop().show(333);
+					})($hiddenInfo));
+				} // If the panel is already active, do nothing.
 			}).mouseleave(function () {
 				var $this = $(this);
 				
@@ -75,7 +79,7 @@
 				}
 			});
 		});
-    }
+	}
 })(jQuery);/**********************************************************************************************************************
  CUSTOM JQUERY-BASED DYNAMIC CONTENT
  *********************************************************************************************************************/

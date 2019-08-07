840581f908aee7cbd59318c0f4dce58f4915b755
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2019-08-07 13:58:59 -0700

    Refactor tasks for automating CSS and JS builds
    Refactor tasks to utilized the latest build method, which relies on a dependendcy imported through the master OUE JS development submodule WSU-UE---JS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,65 @@
+/*!*************************************************************************************************
+ * gulpfile.js
+ * -------------------------------------------------------------------------------------------------
+ * SUMMARY: Gulp automation task definition file for setting up tasks that build CSS and JS files
+ * for use on the WSUWP website of the WSU LSAMP program.
+ *
+ * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ *
+ * REPOSITORY: https://github.com/invokeImmediately/nsse.wsu.edu
+ *
+ * LICENSE: ISC - Copyright (c) 2019 Daniel C. Rieck.
+ *
+ *   Permission to use, copy, modify, and/or distribute this software for any purpose with or
+ *   without fee is hereby granted, provided that the above copyright notice and this permission
+ *   notice appear in all copies.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+ *   SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+ *   DANIEL RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ *   DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *   PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// TABLE OF CONTENTS
+// -----------------
+// §1: Gulp task dependencies..................................................................40
+// §2: Specificiation of build settings .......................................................45
+//   §2.1: getCssBuildSettings()...............................................................48
+//   §2.2: getJsBuildSettings()................................................................83
+// §3: Entry point: Set up of build taks......................................................110
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+( function() {
+
 'use strict';
 
-/* -------------------------------------------------------------------------------------------------
-** Variable Declarations
-*/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §1: Gulp task dependencies
 
-// Gulp task dependencies
-var cleanCss = require( 'gulp-clean-css' );
-var concat = require( 'gulp-concat' );
-var extName = require( 'gulp-extname' );
-var gcmq = require( 'gulp-group-css-media-queries' );
-var gulp = require( 'gulp' );
-var insert = require( 'gulp-insert' );
-var insertLines = require( 'gulp-insert-lines' );
-var lessc = require( 'gulp-less' );
-var replace = require( 'gulp-replace' );
-var uglifyJs = require( 'gulp-uglify' );
-var pump = require( 'pump' );
+var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
 
-/* -------------------------------------------------------------------------------------------------
-** Function declarations
-*/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §2: Specificiation of build settings 
+
+////////
+// §2.1: getCssBuildSettings()
+
+/**
+ * Get the settings for a gulp-mediated custom CSS build from Less source files.
+ *
+ * @return {object} - Instance of gulpBuilder.CssBuildSettings.
+ */
 
 function getCssBuildSettings() {
-	return {
-		commentRemovalNeedle: /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm,
-		dependenciesPath: './WSU-UE---CSS/',
-		destFolder: './CSS/',
-		fontImportStr: '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:300|Robot\
-o+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n',
-		insertingMediaQuerySectionHeader: {
+	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
+	var dependenciesPath = './WSU-UE---CSS/';
+	var destFolder = './CSS/';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|\
+Roboto+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n';
+	var insertingMediaQuerySectionHeader = {
 			'before': /^@media/,
 			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════\
 ════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ######################\
@@ -36,15 +67,26 @@ o+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n',
 ═══════════════════════════════════════════════════════════════════════════════════════════════════\
 ═════════════╝\r\n*/',
 			'stopAfterFirstMatch': true
-		},
-		minCssFileExtension: '.min.css',
-		minCssFileHeaderStr: '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Pleas\
-e see [https://github.com/invokeImmediately/learningcommunities.wsu.edu] for a repository of source\
- code. */\r\n',
-		sourceFile: './CSS/learningcommunities-custom.less'
-	};
+		};
+	var minCssFileExtension = '.min.css';
+	var minCssFileHeaderStr = '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Plea\
+se see [https://github.com/invokeImmediately/learningcommunities.wsu.edu] for a repository of sourc\
+e code. */\r\n';
+	var sourceFile = './CSS/learningcommunities-custom.less';
+
+	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, dependenciesPath,
+		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
+		minCssFileHeaderStr, sourceFile);
 }
 
+////////
+// §2.2: getJsBuildSettings()
+
+/**
+ * Get the settings for a gulp-mediated custom JS build.
+ *
+ * @return {object} - Simple collection of settings for JS builds.
+ */
 function getJsBuildSettings() {
 	return {
 		buildDependenciesList: [
@@ -52,74 +94,22 @@ function getJsBuildSettings() {
 			'./WSU-UE---JS/jQuery.cookieObjs.js',
 			'../qTip2/dist/jquery.qtip.min.js',
 			'./WSU-UE---JS/jQuery.qTip.js',
+			'./WSU-UE---JS/jQuery.css-data.js',
 			'./WSU-UE---JS/jQuery.textResize.js',
-			'../imagesloaded/imagesloaded.pkgd.min.js',
-			'../masonry/dist/masonry.pkgd.min.js',
-			'./WSU-UE---JS/jQuery.masonry-custom.js',
 			'./JS/fyf-custom.js'
 		],
 		commentNeedle: /^(\/\*)(?!!)/g,
 		compiledJsFileName: 'fyf-build.js',
 		destFolder: './JS/',
 		minJsFileExtension: '.min.js',
-		replaceCallback: fixFileHeaderComments
+		replaceCallback: gulpBuilder.fixFileHeaderComments
 	};
 }
 
-function fixFileHeaderComments ( match, p1, offset, string ) {
-	var replacementStr = match;
-	if ( offset == 0 ) {
-		replacementStr = '/*!';
-	}
-	return replacementStr;
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §3: Entry point: Set up of build taks
 
-function setUpCssBuildTask( settings ) {
-	gulp.task( 'buildMinCss', function ( callBack ) {
-		pump( [
-				gulp.src( settings.sourceFile ),
-				lessc( {
-					paths: [settings.dependenciesPath]
-				} ),
-				replace( settings.commentRemovalNeedle, '' ),
-				insert.prepend( settings.fontImportStr ),
-				insert.prepend( settings.minCssFileHeaderStr ),
-				gulp.dest( settings.destFolder ),
-				gcmq(),
-				insertLines( settings.insertingMediaQuerySectionHeader ),
-				cleanCss(),
-				extName( settings.minCssFileExtension ),
-				gulp.dest( settings.destFolder )
-			],
-			callBack
-		);
-	} );
-}
+gulpBuilder.setUpCssBuildTask( getCssBuildSettings() );
+gulpBuilder.setUpJsBuildTask( getJsBuildSettings() );
 
-function setUpJsBuildTask( settings ) {
-	gulp.task( 'buildMinJs', function ( callBack ) {
-		pump( [
-				gulp.src( settings.buildDependenciesList ),
-				replace( settings.commentNeedle, settings.replaceCallback ),
-				concat( settings.compiledJsFileName ),
-				gulp.dest( settings.destFolder ),
-				uglifyJs( {
-					output: {
-						comments: /^!/
-					},
-					toplevel: true,
-				} ),
-				extName( settings.minJsFileExtension ),
-				gulp.dest( settings.destFolder )
-			],
-			callBack
-		);
-	} );
-}
-
-/* -------------------------------------------------------------------------------------------------
-** Main execution sequence
-*/
-
-setUpCssBuildTask( getCssBuildSettings() );
-setUpJsBuildTask( getJsBuildSettings() );
+} )();

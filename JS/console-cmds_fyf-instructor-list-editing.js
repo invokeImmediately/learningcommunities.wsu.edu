265d9bdf86169ae71251897b263fa8e3144798ac
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2019-08-07 16:55:09 -0700

    Add console commands for adjusting WSUWP interface
    Commands added are for using jQuery to adjust the WSUWP editor's TablePress interface for lengthening rows and widening columns when updating First-Year Focus course and instructor information.

---
+++ JS/console-cmds_fyf-instructor-list-editing.js
@@ -0,0 +1,32 @@
+// Console commands for utilizing jQuery to expand TablePress rows and columns when editing the list
+// of First-Year Focus instructors.
+var $editFormBody = jQuery('#edit-form-body');
+var $tds = $editFormBody.find('td');
+$tds.css('min-height', '8em');
+$tds.css('height', '8em');
+var $tds3 = $editFormBody.find('td:nth-child(3)');
+$tds3.css('width', '480px');
+var $tds4 = $editFormBody.find('td:nth-child(4)');
+$tds4.css('width', '288px');
+var $tds5 = $editFormBody.find('td:nth-child(5)');
+$tds5.css('width', '288px');
+var $tds6 = $editFormBody.find('td:nth-child(6)');
+$tds6.css('width', '288px');
+
+
+// Console commands for utilizing jQuery to expand TablePress rows and columns when editing the list
+// of First-Year Focus courses.
+var $editFormBody = jQuery('#edit-form-body');
+var $tds = $editFormBody.find('td');
+$tds.css('min-height', '6em');
+$tds.css('height', '6em');
+var $tds3 = $editFormBody.find('td:nth-child(3)');
+$tds3.css('width', '288px');
+var $tds4 = $editFormBody.find('td:nth-child(4)');
+$tds4.css('width', '120px');
+var $tds5 = $editFormBody.find('td:nth-child(5)');
+$tds5.css('width', '120px');
+var $tds6 = $editFormBody.find('td:nth-child(6)');
+$tds6.css('width', '288px');
+var $tds7 = $editFormBody.find('td:nth-child(7)');
+$tds7.css('width', '480px');
